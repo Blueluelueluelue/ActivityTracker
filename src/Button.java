d529b4ed9f57abcda9374b52ffe2b25d752fdac9
Blueluelueluelue <tahaking786@gmail.com>
2018-10-05 17:53:09 +0530

    Can check if a point is inside the button

--- src/Button.java
+++ src/Button.java
@@ -8,7 +8,7 @@ public class Button {
     private int bottomRightY;
     private int fillColor;
     private int borderColor;
-    private int curve;
+    private int cornerRadius;
 
     public Button(int x1, int y1, int x2, int y2, PApplet pAppletObj) {
         this.pAppletObj = pAppletObj;
@@ -16,9 +16,9 @@ public class Button {
         topLeftY = y1;
         bottomRightX = x2;
         bottomRightY = y2;
-        fillColor = pAppletObj.color(230);
-        borderColor = pAppletObj.color(60);
-        curve = 10;
+        fillColor = pAppletObj.color(220);
+        borderColor = pAppletObj.color(0);
+        cornerRadius = PApplet.floor((0.2f * (x2 - x1)));
     }
 
     public Button(int x1, int y1, int x2, int y2, int fillColor, int borderColor, PApplet pAppletObj) {
@@ -31,7 +31,13 @@ public class Button {
         pAppletObj.pushMatrix();
         pAppletObj.fill(fillColor);
         pAppletObj.stroke(borderColor);
-        pAppletObj.rect(topLeftX, topLeftY, bottomRightX, bottomRightY, curve);
+        pAppletObj.rect(topLeftX, topLeftY, bottomRightX, bottomRightY, cornerRadius);
         pAppletObj.popMatrix();
+    }
+
+    public boolean isInside(int pointX, int pointY) {
+        boolean xIsInside = pointX > topLeftX && pointX < bottomRightX + cornerRadius;
+        boolean yIsInside = pointY > topLeftY && pointY < bottomRightY + cornerRadius;
+        return xIsInside && yIsInside;
     }
 }
